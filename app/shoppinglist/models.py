f507abcd9a936ba589e694cbd82bcd67541feee2
robert.cheng <robert.cheng@moven.com>
2016-11-21 03:37:34 -0500

    database created

--- app/shoppinglist/models.py
+++ app/shoppinglist/models.py
@@ -3,3 +3,20 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    uid = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=20)
+    age = models.IntegerField()
+
+class Items(models.Model):
+    item_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=20)
+    category = models.CharField(max_length=20)
+    price = models.IntegerField()
+
+class Shopping(models.Model):
+    item_id = models.ForeignKey(Customer)
+    uid = models.ForeignKey(Items)
+    amount = models.IntegerField()
+
