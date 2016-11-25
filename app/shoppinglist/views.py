28b9f5d84211b6ea5c7363468dfcf0374d8aedb2
Robert Cheng <chengchang8@gmail.com>
2016-11-25 16:24:16 -0500

    get request

--- app/shoppinglist/views.py
+++ app/shoppinglist/views.py
@@ -1,4 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-        return HttpResponse("Hello, world. You're at the polls index.")
+        return HttpResponse("<p>In Index View</p>")
+
+def item_detail(request, id):
+	return HttpResponse('<p>In item_detail view with id {0}</p>'.format(id))
+
+def customer_detail(request, id):
+	return HttpResponse('<p>In customer_detail view with id {0}</p>'.format(id))
+
+def list_detail(request, id):
+	return HttpResponse('<p>In list_detail view with id {0}</p>'.format(id))
